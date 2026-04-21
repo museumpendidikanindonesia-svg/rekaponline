52bab9f8636408d045dac0c40398085dac8fc952
museum pendidikan indonesia <museumpendidikanindonesia@gmail.com>
2026-04-21 12:00:08 +0700

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,13 @@
+let data = JSON.parse(localStorage.getItem("mpiData")) || [];
+
+const harga = {
+  Umum:10000,
+  Sabak:10000,
+  Paket:35000,
+  Grip:50000,
+  Ongotan:100000
+};
+
+function saveData(){
+  localStorage.setItem("mpiData", JSON.stringify(data));
+}
